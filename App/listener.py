9095534ced78f22a40b48545b98fb70d2330c23d
Arce <gonzarce@a45e60c75eb3.ant.amazon.com>
2017-03-18 22:35:29 +0000

    added 404 Not Found error handling

--- App/listener.py
+++ App/listener.py
@@ -1,9 +1,8 @@
-from flask import Flask
+from flask import Flask, abort
 from topgames import topgames
 import json
 
 app = Flask(__name__)
-
 
 # Define the first method to retrieve the gamelist of top PS4 Games
 @app.route('/games', methods=['GET'])
@@ -22,6 +21,7 @@ def allgames():
     # Return our pretty printed JSON response
     return response
 
+
 # Define the second method to retrieve only the given Game Title as <game_title>
 @app.route('/games/<game_title>', methods=['GET'])
 def singlegame(game_title):
@@ -29,9 +29,15 @@ def singlegame(game_title):
     gamelist = topgames()
 
     # Grab only the object we called
-    game = [ d for d in gamelist if d['title'] == game_title ]
+    game = [ g for g in gamelist if g['title'] == game_title ]
 
-     # Build response and set the right mimetype header
+    # Do some error handling
+    if game == []:
+        abort(404)
+    else:
+        game = game[0]
+    
+    # Build response and set the right mimetype header
     response = app.response_class(
         response = json.dumps(game, indent = 2),
         status = 200,
@@ -46,6 +52,6 @@ def singlegame(game_title):
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
+        debug=True,
         port=8888,
     )
-
